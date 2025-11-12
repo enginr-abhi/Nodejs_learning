c6fcdc0e033a02cf1fda54e577730f89bbf6b69f
Abhishek Kumar Thakur <abhishekkumar.thakur@lft.com>
2025-11-12 10:07:08 +0530

    learning till ch-14 dynamic_path

--- ch-14 dynamic_path/models/home.js
+++ ch-14 dynamic_path/models/home.js
@@ -2,6 +2,9 @@
 const fs = require("fs");
 const path = require("path");
 const rootDir = require("../utils/pathUtil");
+const Favourite = require("./favourite");
+
+const homeDataPath = path.join(rootDir, "data", "homes.json");
 
 module.exports = class Home {
   constructor(houseName, price, location, rating, photoUrl) {
@@ -14,8 +17,14 @@ module.exports = class Home {
 
   save() {
     Home.fetchAll((registeredHomes) => {
-      registeredHomes.push(this);
-      const homeDataPath = path.join(rootDir, "data", "homes.json");
+      if (this.id) { // edit home case
+        registeredHomes = registeredHomes.map(home => 
+          home.id === this.id ? this : home);
+      } else { // add home case
+        this.id = Math.random().toString();
+        registeredHomes.push(this);
+      }
+      
       fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), (error) => {
         console.log("File Writing Concluded", error);
       });
@@ -23,9 +32,24 @@ module.exports = class Home {
   }
 
   static fetchAll(callback) {
-    const homeDataPath = path.join(rootDir, "data", "homes.json");
     fs.readFile(homeDataPath, (err, data) => {
       callback(!err ? JSON.parse(data) : []);
     });
+  }
+
+  static findById(homeId, callback) {
+    this.fetchAll(homes => {
+      const homeFound = homes.find(home => home.id === homeId);
+      callback(homeFound);
+    })
+  }
+
+  static deleteById(homeId, callback) {
+    this.fetchAll(homes => {
+      homes = homes.filter(home => home.id !== homeId);
+      fs.writeFile(homeDataPath, JSON.stringify(homes), error => {
+        Favourite.deleteById(homeId, callback);
+      });
+    })
   }
 };
